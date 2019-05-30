aa43ecb77c64cd484199e0fe1cbe5f63fa994810
PARKHONGSOON <kongsooni39@gmail.com>
2019-05-31 00:46:02 +0900

    add hero detail

---
+++ stdng02TourOfHeroes/src/app/mock-heroes.ts
@@ -0,0 +1,20 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+
+  { id: 11, name: '캡틴 아메리카' },
+  { id: 12, name: '헐크' },
+  { id: 13, name: '스파이더맨' },
+  { id: 14, name: '팔콘' },
+  { id: 15, name: '윈터솔져' },
+  { id: 16, name: '블랙 위도우' },
+  { id: 17, name: '호크아이' },
+  { id: 18, name: '닥터 스트레인지' },
+  { id: 19, name: '블랙팬서' },
+  { id: 20, name: '토르' },
+  { id: 21, name: '스타로드' },
+  { id: 22, name: '그루트' },
+  { id: 23, name: '드랙스' },
+  { id: 24, name: '앤트맨' }
+
+];
