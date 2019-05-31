dc315d89e8b3b2affc80c0460cb7ba4276ecd034
PARKHONGSOON <kongsooni39@gmail.com>
2019-05-31 17:58:40 +0900

    Make Project

---
+++ stdng-back/src/test/java/com/study/stdngback/StdngBackApplicationTests.java
@@ -0,0 +1,16 @@
+package com.study.stdngback;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StdngBackApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
